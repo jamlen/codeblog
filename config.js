38b1a286e1246ac0419ae8fe31fa45ea85b69778
James Allen <code@james-allen.co.uk>
2014-05-15 22:03:20 +0100

    Setting up config

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 var crypto = require('crypto');
 
 module.exports = {
-  email: 'hello@martinrue.com',
-  title: 'Blog Title Here',
-  style: 'monokai'
+  email: 'code@james-allen.co.uk',
+  title: 'James Allen',
+  subtitle: 'My random code ramblings...',
+  style: 'tomorrow-night'
 };
 
 var gravatarHash = crypto.createHash('md5').update(module.exports.email).digest('hex');
